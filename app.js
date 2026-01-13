ad6fe247ef93752eab6fa1848ecbffcbcd306390
Nick Burgan <nick.burgan@exygy.com>
2026-01-13 10:09:41 -0800

    Don't announce if we aren't deploying a new rev
    Because the service gets restarted often when system updates are automatically applied, this results in a lot of spam in the admin room. Only announce if we're running on a rev different than we were before.

--- app.js
+++ app.js
@@ -4,13 +4,15 @@ const path = require('path')
 const {loadBrain} = require('./utils/brain.js')
 const {ADMIN_ROOMS} = require('./utils/constants.js')
 const process = require('process')
-const {exec} = require('child_process')
+const {execSync} = require('child_process')
 
 let secrets = {
   SLACK_BOT_TOKEN: '',
   SLACK_SIGNING_SECRET: '',
   SLACK_APP_TOKEN: '',
 }
+
+const REV_FILE = path.join(`${__dirname}/..`, 'last_rev.txt')
 
 async function loadAllSecrets() {
   const {SecretsManagerClient, ListSecretsCommand, GetSecretValueCommand} =
@@ -33,6 +35,18 @@ async function loadAllSecrets() {
     console.log(`Loaded secret ${secret}`)
   })
   await Promise.all(secretPromises)
+}
+
+function getLastRev() {
+  try {
+    return fs.readFileSync(REV_FILE, 'utf8').trim()
+  } catch (e) {
+    return null // first run / missing file
+  }
+}
+
+function setLastRev(rev) {
+  fs.writeFileSync(REV_FILE, rev)
 }
 
 async function startApp() {
@@ -75,24 +89,25 @@ async function startApp() {
     }
   })
 
-  let rev
-  exec('git rev-parse HEAD', (err, stdout) => {
-    if (err) {
-      console.error(`Error executing git rev-parse: ${err}`)
-      rev = 'unknown'
-    } else {
-      rev = stdout.trim()
-    }
-  })
+  let rev = 'unknown'
+  try {
+    rev = execSync('git rev-parse HEAD', {encoding: 'utf8'}).trim()
+  } catch (err) {
+    console.error(`Error executing git rev-parse: ${err}`)
+  }
+  const announce = getLastRev() !== rev
+  setLastRev(rev)
 
   await app.start()
 
-  for (const room of ADMIN_ROOMS) {
-    await app.client.chat.postMessage({
-      token: secrets.SLACK_BOT_TOKEN,
-      channel: room,
-      text: '⚡️ CiviBot is running at revision ' + rev,
-    })
+  if (announce) {
+    for (const room of ADMIN_ROOMS) {
+      await app.client.chat.postMessage({
+        token: secrets.SLACK_BOT_TOKEN,
+        channel: room,
+        text: '⚡️ CiviBot is running at revision ' + rev,
+      })
+    }
   }
   console.log('⚡️ CiviBot is running at revision ' + rev)
 }
